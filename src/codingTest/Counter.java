d3e98e80cfdcaed2cf839b6f6d8fcfd965b1af17
JORDAN ELPERN-WAXMAN <jelpern@users.noreply.github.com>
2015-03-26 19:48:14 -0400

    Fixed bug that was introduced by trying to put the Counter entrySet() directly in the TreeSet.

--- src/codingTest/Counter.java
+++ src/codingTest/Counter.java
@@ -1,6 +1,8 @@
 package codingTest;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Counter<String> {
 	
@@ -12,6 +14,10 @@ public class Counter<String> {
 
 	public int count(String t) {
 		return counts.getOrDefault(t, 0);
+	}
+	
+	public Set<Map.Entry<String, Integer>> items() {
+		return counts.entrySet();
 	}
 	
 }
--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -10,6 +10,7 @@ import org.jsoup.select.Elements;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.SortedSet;
@@ -18,20 +19,18 @@ import java.util.TreeSet;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO take input from command line
 		String url = "http://www.cutestpaw.com/";
 		try {
 			Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+			// Counter is a wrapper around a hash table to mirror the   
 			Counter<String> classCounter = new Counter<String>();
 			Elements allElements = doc.getAllElements(); 
 			// count how many of each class
 			for (Element e: allElements){
 				Set<String> classes = e.classNames();
+				// necessary because some elements have more than one class
 				for (String c: classes){
-					if (c.contains("top")){
-						System.out.println(e.tag());
-						System.out.println("class=" + c + "\"");
-					}
 					classCounter.add(c);
 				}
 				String classString = e.className();
@@ -39,20 +38,21 @@ public class Main {
 					classCounter.add(classString);
 				}
 			}
-			Set<Map.Entry<String, Integer>> classes = classCounter.counts.entrySet(); // the set of entries in classCounter
+			// take all the items out of the counter
+			Set<Map.Entry<String, Integer>> classes = classCounter.items();
 			// create a data store that efficiently and automatically sorts
 			TreeSet<KeyValuePair> sortedClasses = new TreeSet();
-			// put everything in it
+			// transform each entry the counter into a key value pair that sorts on *value*,
+			// and put everything in it
 			for (Map.Entry<String, Integer> entry: classes){
 				KeyValuePair kvp = new KeyValuePair<String, Integer>(entry.getKey(),entry.getValue());
 				sortedClasses.add(kvp);
 			}
 			NavigableSet<KeyValuePair> it = sortedClasses.descendingSet();
 			for (KeyValuePair kv: it){
-				System.out.println(kv); 
+				System.out.println(kv);  
 			}
 			// TODO pop items off the Iterator, check for link, image, text
-			System.out.println(it);
 			System.out.println();
 			//System.out.println(doc);
 		}
