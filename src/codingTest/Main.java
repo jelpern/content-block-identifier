b3ad9684b59fd39967587668efd6647a50bc273c
JORDAN ELPERN-WAXMAN <jelpern@users.noreply.github.com>
2015-03-27 22:39:10 -0400

    Final version Thursday night 3/26.

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -23,34 +23,43 @@ public class Main {
 		String url = "http://www.cutestpaw.com/";
 		try {
 			Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
-			// Counter is a wrapper around a hash table to mirror the   
+			// Counter is a wrapper around a hash table to mirror the Python Counter class 
 			Counter<String> classCounter = new Counter<String>();
 			Elements allElements = doc.getAllElements(); 
+			
 			// count how many of each class
 			for (Element e: allElements){
 				Set<String> classes = e.classNames();
 				// necessary because some elements have more than one class
+				// note that classCounter is not currently implemented with a sorted class
+				// this is because the order can change with every insertion
+				// it would be easy to change this however in the implementation
 				for (String c: classes){
-					classCounter.add(c);
+					// items which don't have a class are represented by empty strings
+					// we don't want to count them
+					if (!c.isEmpty()) {
+						classCounter.add(c);
+					}
 				}
 				String classString = e.className();
 				if (classString.contains(" ")){
 					classCounter.add(classString);
 				}
 			}
+			
 			// take all the items out of the counter
 			Set<Map.Entry<String, Integer>> classes = classCounter.items();
 			// create a data store that efficiently and automatically sorts
 			TreeSet<KeyValuePair> sortedClasses = new TreeSet();
 			// transform each entry the counter into a key value pair that sorts on *value*,
-			// and put everything in it
+			// and put everything in the sorted data store
 			for (Map.Entry<String, Integer> entry: classes){
 				KeyValuePair kvp = new KeyValuePair<String, Integer>(entry.getKey(),entry.getValue());
 				sortedClasses.add(kvp);
 			}
 			NavigableSet<KeyValuePair> it = sortedClasses.descendingSet();
 			for (KeyValuePair kv: it){
-				System.out.println(kv);  
+
 			}
 			// TODO pop items off the Iterator, check for link, image, text
 			System.out.println();
