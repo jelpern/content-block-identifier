88979b50c33cbc78e3722394785365e045a26503
JORDAN ELPERN-WAXMAN <jelpern@users.noreply.github.com>
2015-03-30 00:54:06 -0400

    Got rid of all of the old Counter object fields and methods. Moved counting to the Counter object in a new method called countContentElements that takes a url string and returns a HashMap of <String selector, TripleLiftSelector tls> where tls has the correct counts for everything.

--- src/codingTest/Counter.java
+++ src/codingTest/Counter.java
@@ -1,29 +1,74 @@
 package codingTest;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Counter extends HashMap<String, TripleLiftSelector> {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Counter {
 	
-	final HashMap<TripleLiftSelector, Integer> counts = new HashMap<>();
-
-	public TripleLiftSelector add(TripleLiftSelector t) {
-		int c = counts.merge(t, 1, Integer::sum);
-		t.setCount(counts.get(t));
-		if (c != t.getCount()) { // conditional breakpoint hack 
-			System.out.println("Element count went wrong on item: " + t.getSelector());
-		} 
-		return t;
-		// do we need to return t?
-	}
-
-	public int count(TripleLiftSelector t) {
-		return counts.getOrDefault(t, 0);
+	public Counter () {
+		
 	}
 	
-	public Set<Map.Entry<TripleLiftSelector, Integer>> items() {
-		return counts.entrySet();
+	public HashMap<String, TripleLiftSelector> countContentElements(String url) 
+			throws IOException {
+		Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+		HashMap<String, TripleLiftSelector> selectorCounter = new HashMap<>();
+		Elements allElements = doc.getAllElements(); 
+		
+		// count how many of each tag.class selector
+		for (Element e: allElements) {
+			Set<String> classes = e.classNames();
+			// for loop necessary because some elements have more than one class
+			boolean elementIsContent = TripleLiftSelector.isContent(e);
+			for (String c: classes) {
+				// conditional breakpoint hack - does nothing
+				String selector = e.tagName();
+				// check necessary because some elements have no class attribute
+				if (!c.isEmpty()) {
+					selector += "." + c;
+				}
+				if (selectorCounter.containsKey(selector)) {
+					selectorCounter.get(selector).incrementCount();
+				} else {	
+					TripleLiftSelector tls = new TripleLiftSelector(selector, 1);
+					selectorCounter.put(selector, tls);
+				}
+				TripleLiftSelector tls = selectorCounter.get(selector);
+				if (elementIsContent) tls.incrementNumWithContent();
+				else tls.incrementNumWithoutContent();
+			}
+			// some elements have multiple classes
+			// count how many elements have the same combination of classes
+			if (classes.size() > 1) {
+// 					String selector = e.tagName() + "." + String.join(".",e.className().split("\\s+"));
+				// Above is elegant, but not sure if it handles all types of whitespace in the className string. 
+				// Better safe than sorry
+				String selector = e.tagName();
+				String classSeparator = ".";
+				for (String c: classes){
+					selector += classSeparator + c;
+				}
+				if (selectorCounter.containsKey(selector)) {
+					selectorCounter.get(selector).incrementCount();
+				} else {	
+					TripleLiftSelector tls = new TripleLiftSelector(selector, 1);
+					selectorCounter.put(selector, tls);
+				}
+				TripleLiftSelector tls = selectorCounter.get(selector);
+				if (elementIsContent) tls.incrementNumWithContent();
+				else tls.incrementNumWithoutContent();
+
+			}
+		} // done counting tag.class selectors
+		return selectorCounter;
 	}
+
 	
 }
--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -1,16 +1,9 @@
 package codingTest;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.HashMap;
 import java.util.NavigableSet;
-import java.util.Set;
 import java.util.TreeSet;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class Main {
 
@@ -21,66 +14,19 @@ public class Main {
 		}
 		String url = args[0];
 		try {
-			Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
-			// Counter is a wrapper around a hash table to mirror the Python Counter class
-			// TODO replace Counter with a simple hashmap where the key is the selector string and the value is the tls
-			Counter selectorCounter = new Counter();
-			Elements allElements = doc.getAllElements(); 
-			
-			// count how many of each tag.class selector
-			for (Element e: allElements) {
-				Set<String> classes = e.classNames();
-				// for loop necessary because some elements have more than one class
-				boolean elementIsContent = TripleLiftSelector.isContent(e);
-				for (String c: classes) {
-					// conditional breakpoint hack - does nothing
-					String selector = e.tagName();
-					// check necessary because some elements have no class attribute
-					if (!c.isEmpty()) {
-						selector += "." + c;
-					}
-					if (selectorCounter.containsKey(selector)) {
-						selectorCounter.get(selector).incrementCount();
-					} else {	
-						TripleLiftSelector tls = new TripleLiftSelector(selector, 1);
-						selectorCounter.put(selector, tls);
-					}
-					TripleLiftSelector tls = selectorCounter.get(selector);
-					if (elementIsContent) tls.incrementNumWithContent();
-					else tls.incrementNumWithoutContent();
-				}
-				// some elements have multiple classes
-				// count how many elements have the same combination of classes
-				if (classes.size() > 1) {
-// 					String selector = e.tagName() + "." + String.join(".",e.className().split("\\s+"));
-					// Above is elegant, but not sure if it handles all types of whitespace in the className string. 
-					// Better safe than sorry
-					String selector = e.tagName();
-					String classSeparator = ".";
-					for (String c: classes){
-						selector += classSeparator + c;
-					}
-					if (selectorCounter.containsKey(selector)) {
-						selectorCounter.get(selector).incrementCount();
-					} else {	
-						TripleLiftSelector tls = new TripleLiftSelector(selector, 1);
-						selectorCounter.put(selector, tls);
-					}
-					TripleLiftSelector tls = selectorCounter.get(selector);
-					if (elementIsContent) tls.incrementNumWithContent();
-					else tls.incrementNumWithoutContent();
-
-				}
-			} // done counting tag.class selectors
+			// count the number of each element and whether they contain content
+			Counter myCounter = new Counter();
+			HashMap<String, TripleLiftSelector> selectorCounter = myCounter.countContentElements(url);
 			
 			// now that we're done counting, sort
 			NavigableSet<TripleLiftSelector> it = new TreeSet<>(selectorCounter.values()).descendingSet();
+			
+			// 
 			System.out.println("tag.className,number of occurences,number that are content,number that are not content");
 			for (TripleLiftSelector tls: it){
 				System.out.println(tls.getSelector() + "," + tls.getCount() + "," + tls.getNumWithContent() + "," + tls.getNumWithoutContent());
 			}
 			System.out.println();
-			//System.out.println(doc);
 		}
 		catch (IOException e) {
 			System.out.println("Error while trying to open " + url);
